5727e0e5cff76a093d671aa1be84fedc934778ea
Mekan <mekan2678@gmail.com>
2021-03-03 06:00:27 +0300

    'homevork'

---
+++ ex3.py
@@ -0,0 +1,10 @@
+users = int(input('Введите число: ')) # запрос от пользователя
+
+if users == 1:
+    print(f'{users} процент')
+elif users >= 2 >= 4:
+    print(f'{users} процента')
+else:
+    print(f'{users} процентов')
+
+
